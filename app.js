e0123b01dc59b65729385cc74422778d8a4d7288
raed-bash <raed09321@gmail.com>
2023-07-10 08:55:16 +0300

    linked with mySQL

--- app.js
+++ app.js
@@ -1,93 +1,28 @@
 import http from "http";
-import fs from "fs";
+import mysql from "mysql";
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test1",
+});
+
 http
   .createServer(function (req, res) {
-    console.log(req.method);
-    console.log(req.headers);
-
-    var url = req.url;
-    var users = JSON.parse(fs.readFileSync("users.json"));
     var method = req.method.toUpperCase();
-
-    console.log(req.query);
-    if (method === "OPTIONS") {
-      res.writeHead(204, "No Content", {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": `${req.headers["access-control-request-method"]}, GET, OPTIONS`,
-        "Access-Control-Allow-Headers": `X-PINGOTHER, ${req.headers["access-control-request-headers"]}`,
-      });
-      res.end();
-    } else if ((url === "/users" || url === "/users/") && method === "GET") {
-      response(res, 200, "ok", users);
-      return;
+    var url = req.url;
+    console.log(method);
+    console.log(req.headers);
+    if (url === "/users" && method === "GET") {
+      getUsers(req, res);
+    } else if (url === "/users" && method === "POST") {
+      postUser(req, res);
+    } else {
+      response(res, 404, "Page Not Found", {});
     }
-    // else if ((url === "/users" || url === "/users/") && method === "PATCH") {
-    //   console.log("post is runinig");
-    //   let body = "";
-    //   req.on("data", (chunk) => {
-    //     body += chunk.toString();
-    //   });
-    //   req.on("end", () => {
-    //     const data = JSON.parse(body);
-    //     const userIndex = users.findIndex((user) => user.id === data.id);
-    //     users[userIndex] = data;
-    //     fs.writeFileSync("users.json", JSON.stringify(users));
-    //     response(res, 201, "ok", data);
-    //   });
-
-    //   return;
-    // }
-    else if ((url === "/users" || url === "/users/") && method === "POST") {
-      getReqBodyJson(req)
-        .then((newUser) => {
-          newUser.id = 0;
-          const newId =
-            users.length > 0
-              ? Math.max(...users.map((user) => user.id)) + 1
-              : 1;
-          newUser = { ...newUser, id: newId };
-          users.push(newUser);
-          fs.writeFileSync("users.json", JSON.stringify(users));
-          response(res, 201, "ok", newUser);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      return;
-    }
-    //   else if (url.startsWith("/users/") && method === "GET") {
-    //   let userId = parseInt(url.substring("/users/".length));
-    //   let user = users.find((user) => user.id === userId);
-    //   response(res, 200, "ok", user);
-    //   return;
-    // } else if (url.startsWith("/users/") && method === "DELETE") {
-    //   let userId = parseInt(url.substring("/users/".length));
-    //   if (userId) {
-    //     let userIndex = users.findIndex((user) => user.id == userId);
-    //     response(res, 200, "ok", users[userIndex]);
-    //     users.splice(userIndex, 1);
-    //   } else {
-    //     getReqBodyJson(req)
-    //       .then((obj) => {
-    //         console.log(obj);
-    //       })
-    //       .catch((error) => {
-    //         console.log(error);
-    //       });
-    //   }
-    //   return;
-    // }
-    else response(res, 404, "not Found page", {});
   })
   .listen(8080);
-function response(res, status, message, data) {
-  res.writeHead(status, message, {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-  });
-  res.end(JSON.stringify(data));
-}
+
 function getReqBodyJson(req) {
   return new Promise((resolve, reject) => {
     try {
@@ -97,7 +32,6 @@ function getReqBodyJson(req) {
       });
       req.on("end", () => {
         const data = JSON.parse(body);
-
         resolve(data);
       });
     } catch (error) {
@@ -105,29 +39,33 @@ function getReqBodyJson(req) {
     }
   });
 }
+function response(res, status, message, data) {
+  res.writeHead(status, message, {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  });
+  res.end(JSON.stringify(data));
+}
 
-let UserInterface = {
-  id: "number",
-  name: "string",
-  level: "number",
-  items: "array",
-};
-
-let ItemsInterface = {
-  name: "string",
-  number: "number",
-};
-
-function vailedateItems(user) {
-  for (const obj of user.items) {
-    if (typeof obj === "object") {
-      for (const [key, value] of Object.entries(ItemsInterface)) {
-        if (typeof obj?.[key] !== value) {
-          console.log(key, value);
-          return false;
+function getUsers(req, res) {
+  con.query(`SELECT * FROM table1`, function (err, result) {
+    if (err) throw err;
+    console.log("users: " + result);
+    response(res, 200, "OK", result);
+  });
+}
+function postUser(req, res) {
+  getReqBodyJson(req)
+    .then((user) => {
+      con.query(
+        `INSERT INTO table1 (email,password) VALUES ('${user.email}', '${user.password}')`,
+        function (err, result) {
+          if (err) throw err;
+          console.log("Number of records inserted: ");
+          console.log("ID: " + result.insertId);
+          response(res, 200, "OK", { ...user, id: result.insertId });
         }
-      }
-    }
-  }
-  return true;
+      );
+    })
+    .catch((error) => console.log(error));
 }
